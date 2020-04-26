1ce30139cbe7bec9233efa05ad40f0dd30c1654e
Fabian <fabian.poels@gmail.com>
2020-04-26 17:47:39 +0200

    PRELUDE-25 button color fixing

--- src/components/admin/_LayoutButton.vue
+++ src/components/admin/_LayoutButton.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="buttonCell">
-    <b-btn v-if="button" variant="secondary" class="button" v-b-modal="`editItemButton-${page._id}-${x}-${y}`" :style="{ backgroundColor: button.color, borderColor: button.color }">
+    <b-btn v-if="button" variant="secondary" class="button" :class="{ layoutButton: customButton }" v-b-modal="`editItemButton-${page._id}-${x}-${y}`" :style="buttonStyle">
       {{ itemById(button.item).name }}
     </b-btn>
     <b-btn variant="outline-secondary" class="button" v-else v-b-modal="`addItemButton-${page._id}-${x}-${y}`">
@@ -11,6 +11,7 @@
   </div>
 </template>
 <script>
+import chroma from 'chroma-js'
 import { mapGetters } from 'vuex'
 import AddItemButton from '@/components/admin/modals/AddItemButton'
 import EditItemButton from '@/components/admin/modals/EditItemButton'
@@ -51,6 +52,23 @@ export default {
     page() {
       return this.pageById(this.pageId)
     },
+
+    customButton() {
+      return this.button.color && this.button.color !== ''
+    },
+
+    buttonStyle() {
+      if (this.customButton) {
+        let chromaColor = chroma(this.button.color)
+        return {
+          '--color': this.button.color,
+          '--color-hover': chromaColor.darken(0.5),
+          '--text-color': chromaColor.luminance() > 0.5 ? '#000' : '#fff',
+        }
+      } else {
+        return {}
+      }
+    },
   },
 
   methods: {
@@ -79,5 +97,16 @@ export default {
 
 .button:hover > .iconAdd {
   display: inline-block;
+}
+
+.layoutButton {
+  background-color: var(--color);
+  border-color: var(--color);
+  color: var(--text-color);
+}
+
+.layoutButton:hover {
+  background-color: var(--color-hover);
+  border-color: var(--color-hover);
 }
 </style>
